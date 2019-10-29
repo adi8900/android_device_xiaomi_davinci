d185323ba850f4e75925da24445fb201105b410a
mountaser <mountaser1998@gmail.com>
2019-10-30 01:07:59 +0530

    Revert "raphael: Add DC dimming"
    This reverts commit d5e1b10a8e70c5afc4d02988c1d0c2f19137c7bc.

--- parts/Android.mk
+++ parts/Android.mk
@@ -28,7 +28,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v14-preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    vendor.xiaomi.hardware.displayfeature-V1.0-java \
     vendor.xiaomi.hardware.motor-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
